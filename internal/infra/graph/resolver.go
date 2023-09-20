1d0aa79f7dfbf80d056f161f81039c3318a3ea55
Hugo Yamauthi Silva <hugo.yamauthi@gmail.com>
2023-09-19 22:53:49 -0300

    feat: :sparkles: Initial commit

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/yamauthi/goexpert-clean-arch-challenge/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
